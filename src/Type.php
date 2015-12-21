15c7aa14bfd72f0eb774f23b80a23be57950678d
amekusa <post@amekusa.com>
2015-12-21 18:09:50 +0900

    Load main.php via autoloader

--- src/Type.php
+++ src/Type.php
@@ -3,10 +3,10 @@
 class Type {
 
 	/**
-	 * @param string|\stdClass $xType
+	 * @param string|\stdClass $Type
 	 */
-	public static function getInstance($xType) {
-		return new static($xType);
+	public static function getInstance($Type) {
+		return new static($Type);
 	}
 
 	/**
@@ -18,25 +18,25 @@ class Type {
 
 	protected
 		$name,
-		$core;
+		$raw;
 
 	/**
-	 * @param string|\stdClass $xType
+	 * @param string|\stdClass $Type
 	 */
-	protected function __construct($xType) {
-		if (is_string($xType)) $this->name = $xType;
-		if ($xType instanceof \stdClass) {
-			$this->core = $xType;
-			$this->name = $xType->name;
+	protected function __construct($Type) {
+		if (is_string($Type)) $this->name = $Type;
+		if ($Type instanceof \stdClass) {
+			$this->raw = $Type;
+			$this->name = $Type->name;
 		}
 	}
 
-	public function __get($xProp) {
-		return $this->getCore()->$xProp;
+	public function __get($Prop) {
+		return $this->getRaw()->$Prop;
 	}
 
-	public function __set($xProp, $xValue) {
-		$this->getCore()->$xProp = $xValue;
+	public function __set($Prop, $Value) {
+		$this->getRaw()->$Prop = $Value;
 	}
 
 	public function __toString() {
@@ -50,24 +50,24 @@ class Type {
 	/**
 	 * @return \stdClass A post type object
 	 */
-	public function getCore() {
-		if (!$this->core) $this->updateCore();
-		return $this->core;
+	public function getRaw() {
+		if (!$this->raw) $this->updateCore();
+		return $this->raw;
 	}
 
 	protected function updateCore() {
 		if (!post_type_exists($this->name)) $this->register();
-		$this->core = get_post_type_object($this->name);
+		$this->raw = get_post_type_object($this->name);
 	}
 
-	public function reserveRegister(array $xParams = array ()) {
+	public function reserveRegister(array $Params = array ()) {
 		add_action('init', function () {
-			$this->register($xParams);
+			$this->register($Params);
 		});
 	}
 
-	public function register(array $xParams = array ()) {
-		$params = array_merge_recursive(static::getDefaultParams(), $xParams);
+	public function register(array $Params = array ()) {
+		$params = array_merge_recursive(static::getDefaultParams(), $Params);
 		if (!$params) register_post_type($this->name);
 		else register_post_type($this->name, $params);
 	}
