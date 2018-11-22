e62a3368a06e00c1a0fe824bb1891d62f1450778
Unknown <amekusa@fastmail.com>
2018-11-23 02:28:09 +0900

    [WIP] + Registry class

---
+++ src/Registry.php
@@ -0,0 +1,22 @@
+<?php namespace amekusa\wpelib; main::required;
+
+class Registry {
+	private static $instance;
+	private $entries;
+
+	public static function instance() {
+		if (!self::$instance) self::$instance = new self();
+		return self::$instance;
+	}
+
+	private function __construct() {
+		$this->entries = array ();
+	}
+
+	/**
+	 * @param Registerable $Entry
+	 */
+	public function register($Entry) {
+
+	}
+}
